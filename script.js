7fef0bdda89607589461d18c8e903954187159fc
Andrey Cheshko <andreycheshko@gmail.com>
2024-12-05 21:00:55 +0300

    mobile menu end

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
-function toggleMobileMenu(menu) {
-  menu.classList.toggle('open');
-}
+let burger = document.querySelector('.burger');
+let nav = document.querySelector('.nav');
+let navItems = nav.querySelectorAll('.nav__link');
+let header = document.querySelector('.header');
+let headerHeight = header.offsetHeight;
+let overlay = document.querySelector('.overlay');
+
+document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
+
+burger.addEventListener('click', () => {
+  document.body.classList.toggle('stop-scroll');
+  burger.classList.toggle('burger--active');
+  overlay.classList.toggle('overlay--visible');
+
+  let expanded = burger.getAttribute('aria-expanded') === 'true' || false;
+  burger.setAttribute('aria-expanded', !expanded);
+
+  if (nav.classList.contains('nav--visible')) {
+    nav.classList.remove('nav--visible');
+    setTimeout(() => {
+      nav.classList.remove('nav--initial');
+    }, 600);
+  } else {
+    nav.classList.add('nav--initial');
+    nav.classList.add('nav--visible');
+  }
+});
+
+overlay.addEventListener('click', () => {
+  document.body.classList.remove('stop-scroll');
+  burger.classList.remove('burger--active');
+  nav.classList.remove('nav--visible');
+  overlay.classList.remove('overlay--visible');
+  burger.setAttribute('aria-expanded', 'false');
+});
+
+navItems.forEach((el) => {
+  el.addEventListener('click', () => {
+    document.body.classList.remove('stop-scroll');
+    burger.classList.remove('burger--active');
+    nav.classList.remove('nav--visible');
+    overlay.classList.remove('overlay--visible');
+
+    burger.setAttribute('aria-expanded', false);
+  });
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 1024) {
+    nav.style.transition = 'none';
+    nav.classList.remove('nav--visible');
+    nav.classList.remove('nav--initial');
+    overlay.classList.remove('overlay--visible');
+    burger.classList.remove('burger--active');
+    document.body.classList.remove('stop-scroll');
+    burger.setAttribute('aria-expanded', 'false');
+    setTimeout(() => {
+      nav.style.transition = '';
+    }, 0);
+  }
+});
